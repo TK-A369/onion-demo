3ae06c4e2a8fd90cd3b68648622f7641c2b74476
Tomasz Kawalec <tkawalec369@gmail.com>
2023-08-05 14:30:34 +0200

    Move rendering from Window to callback defined by content, and define shaders and render groups in prototypes

--- OnionDemo/Program.cs
+++ OnionDemo/Program.cs
@@ -5,6 +5,8 @@ using OnionEngine.IoC;
 using OnionEngine.DataTypes;
 using OnionEngine.Network;
 
+using OpenTK.Graphics.OpenGL4;
+
 public class TestNetMessage : NetMessage
 {
 	public string content = "";
@@ -94,6 +96,74 @@ class Program
 				};
 				gameManager.AddComponent(spriteComponent);
 			});
+
+			win.renderCallback = () =>
+			{
+				HashSet<Int64> entitiesToRender = gameManager.QueryEntitiesOwningComponents(new HashSet<Type>() { typeof(RenderComponent) });
+				foreach (Int64 entity in entitiesToRender)
+				{
+					Int64 renderComponentId = gameManager.GetComponent(entity, typeof(RenderComponent));
+					RenderComponent renderComponent = (gameManager.components[renderComponentId] as RenderComponent) ?? throw new NullReferenceException();
+					renderComponent.renderData.Clear();
+				}
+
+				win.drawSpritesEvent.Fire(null);
+
+				// Clear render groups' vertices data
+				foreach (RenderGroup renderGroup in win.renderGroups.Values)
+				{
+					renderGroup.vertices.Clear();
+					renderGroup.indices.Clear();
+				}
+
+				// Add vertices to appropriate render groups
+				foreach (Int64 entity in entitiesToRender)
+				{
+					Int64 renderComponentId = gameManager.GetComponent(entity, typeof(RenderComponent));
+					RenderComponent renderComponent = (gameManager.components[renderComponentId] as RenderComponent) ?? throw new NullReferenceException();
+					List<RenderData> dataToRender = renderComponent.GetVertices();
+					foreach (RenderData renderData in dataToRender)
+					{
+						RenderGroup renderGroup = win.renderGroups[renderData.renderGroup];
+						int indexOffset = renderGroup.vertices.Count / 6;
+						foreach (float vertex in renderData.vertices)
+						{
+							renderGroup.vertices.Add(vertex);
+						}
+						foreach (int index in renderData.indices)
+						{
+							renderGroup.indices.Add(index + indexOffset);
+						}
+					}
+				}
+
+				win.offscreenRenderTargets["offscreen-render-target-1"].Clear();
+				foreach (RenderGroup renderGroup in win.renderGroups.Values)
+				{
+					renderGroup.Render(win.offscreenRenderTargets["offscreen-render-target-1"]);
+				}
+
+				// Render to default framebuffer - onscreen
+				GL.BindFramebuffer(FramebufferTarget.Framebuffer, 0);
+				GL.Viewport(0, 0, win.width, win.height);
+
+				// Clear
+				GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+				GL.Clear(ClearBufferMask.ColorBufferBit);
+
+				GL.BindVertexArray(win.vertexArrayObject);
+				GL.BindBuffer(BufferTarget.ArrayBuffer, win.vertexBufferObject);
+				win.shaders["textured-shader"].Use();
+				win.textureAtlases["texture-atlas-1"].Use();
+				win.offscreenRenderTargets["offscreen-render-target-1"].UseTexture();
+				// textures["floor-tile-1"].Use(TextureUnit.Texture0);
+				win.shaders["textured-shader"].SetUniform1i("texture0", 0);
+				GL.DrawArrays(PrimitiveType.Triangles, 0, 6);
+
+
+
+				win.Context.SwapBuffers();
+			};
 
 			win.Run();
 		}
